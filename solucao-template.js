b3a87806adcc28a6f910dcc0cc18bfa611cc802e
Rômulo Wylhames <blackwylhames1001@gmail.com>
2021-10-05 17:53:44 -0300

    Atualização do Readme.md

--- solucao-template.js
+++ solucao-template.js
@@ -1094,51 +1094,199 @@ var listaProdutos = [
 ];
 
 
-/*exercicio 1*/
+                            // agora começam as resoluções
 
-    const totalEstoque = listaProdutos.reduce((prevVal,elem) => prevVal+elem.qtdEstoque, 0 )
-    console.log('Total Estoque',totalEstoque)
+function exercicio1(){
+    var total = 0;
+    for(i=0; i<listaProdutos.length; i++){
+        let produto = listaProdutos[i];
+            total   = total + produto.qtdEstoque;
+    }
+    console.log("Total de produtos em estoque = "+total);
+}
 
-/*exercicio 2*/
+function exercicio2(){
+    var total = 0
+    for (i=0; i<listaProdutos.length; i++){
+        let produto = listaProdutos[i];
+        if (produto.emDestaque== "sim"){
+            total += 1;
+        }}
+        console.log("Total Produtos Em Destaque =" +total)
+}
 
-    const Destaque = listaProdutos.reduce((total, elemento) => {
-        if (elemento.emDestaque === "sim") return total += (elemento.emDestaque =1);
-        else return total}, 0);
-        console.log('Em Destaque',Destaque)
+function exercicio3(){
+    var total = 0
+    for (i=0; i<listaProdutos.length; i++){
+        let produto = listaProdutos[i];
 
-/*exercicio 3*/
+        if (produto.disponivel== "sim"){
+            total += 1;
+        }}
+        console.log("Total Produtos Disponivel =" +total)
+}
 
-    const Disponivel = listaProdutos.reduce((total, elemento) => {
-        if (elemento.disponivel === 'sim') return total += (elemento.disponivel =1);
-        else return total}, 0);
-        console.log('Disponivel',Disponivel)
+function exercicio4(){
+    var total = 0;
+    for (i=0; i<listaProdutos.length; i++){
+        let produto = listaProdutos[i];
 
-/*exercicio 4*/
+        if (produto.emDestaque == "sim" && produto.disponivel == "sim"){
+            total += produto.qtdEstoque;
+        }
+    }
+    console.log("Total de produtos em estoque disponíveis e em destaque = "+total);
+}
+
+function exercicio5(){
+    var total = 0;
+    for(i=0; i<listaProdutos.length; i++){
+        let produto = listaProdutos[i];
+            total   = total + produto.qtdEstoque * produto.preco;
+    }
+    console.log("Valor Total do Inventario = "+total);
+}
+
+function exercicio6(){
+    let produtoMaisCaro = listaProdutos[0];
+    for (i=1; i<listaProdutos.length; i++){
+        let produto = listaProdutos[i];
+        if (produto.preco > produtoMaisCaro.preco){
+            produtoMaisCaro = produto;
+        }
+    }
+    console.log("Produto mais caro da empresa é o  "+produtoMaisCaro.descricao+ " cujo preço é R$ "+produtoMaisCaro.preco);
+}
+
+function exercicio7(){
+    let produtoMaisBarato = listaProdutos[0];
+    for (i=1; i<listaProdutos.length; i++){
+        let produto = listaProdutos[i];
+        if (produto.preco < produtoMaisBarato.preco){
+            produtoMaisBarato = produto;
+        }
+    }
+    console.log("Produto mais barato da empresa é o  "+produtoMaisBarato.descricao+ " cujo preço é R$ "+produtoMaisBarato.preco);
+}
+
+function exercicio8(){
+    let estoqueMaisValioso = listaProdutos[0];
+    for (i=1; i<listaProdutos.length; i++){
+        let produto = listaProdutos[i];
+        if (produto.qtdEstoque > 0){
+            if (produto.preco * produto.qtdEstoque > estoqueMaisValioso.preco * estoqueMaisValioso.qtdEstoque){
+                estoqueMaisValioso = produto;
+            }
+        }        
+    }
+
+    console.log("O estoque mais valioso é do produto "+estoqueMaisValioso.descricao + 
+                " cujo valor está estimado em R$ "+(estoqueMaisValioso.preco*estoqueMaisValioso.qtdEstoque));
+}
+
+function exercicio9(){
+    let estoqueMaisBarato = listaProdutos[0];
+    for (i=1; i<listaProdutos.length; i++){
+        let produto = listaProdutos[i];
+        if (produto.qtdEstoque > 0){
+            if (produto.preco * produto.qtdEstoque < estoqueMaisBarato.preco * estoqueMaisBarato.qtdEstoque){
+                estoqueMaisBarato = produto;
+            }
+        }        
+    }
+    console.log("O estoque mais barato é do produto "+estoqueMaisBarato.descricao + 
+                " cujo valor está estimado em R$ "+(estoqueMaisBarato.preco*estoqueMaisBarato.qtdEstoque));
+}
+
+function exercicio10(){
+    var total = 0;
+    for(i=0; i<listaProdutos.length; i++){
+        let produto = listaProdutos[i];
+            total   = total + produto.qtdEstoque * produto.preco / listaProdutos.length;
+    }
+    console.log("Valor Total do Ticket Medio = "+total);
+}
+
+/*function exercicio11(){
+
+    var listaDeptos = [];
+    // como saber todos os departamentos??
+    let codDepto = 0;
+    for (i=0;i<listaProdutos.length; i++){
+        let produto = listaProdutos[i];
+        if (produto.departamento.idDepto != codDepto){
+            //console.log("Encontrei novo departamento = "+produto.departamento.nomeDepto);
+            // vou definir uma pequena estrutura para armazenar na lista, contendo o nome do departamento e inicialmente a quantidade zerada
+            let itemLista = {
+                nomeDepto     : produto.departamento.nomeDepto,
+                idDepto       : produto.departamento.idDepto,
+                somatoriaItens: 0
+            };
+            listaDeptos.push(itemLista);
+            codDepto = produto.departamento.idDepto;
+        }
+    }
+
+    // agora, vou novamente percorrer a lista e, para cada produto, percorro os departamentos definidos e somo o estoque
+
+    for (i=0; i < listaProdutos.length ; i++){
+        // para cada produto da lista, eu preciso encontrar seu respectivo departamento na outra lista e somar ao seu estoque
+        let produto = listaProdutos[i];
+
+        // tenho que percorrer a lista de departamentos
+        for (j=0;j<listaDeptos.length; j++){
+            if (produto.departamento.idDepto == listaDeptos[j].idDepto){  // o depto do produto corresponde ao depto da lista?
+                listaDeptos[j].somatoriaItens += produto.qtdEstoque;
+                break;
+            }
+        }
+    }
+
+    // depois de tudo, exibo a lista
+    console.log(listaDeptos);
+}
+
+function exercicio13(){
+    var listaDeptos = [];
+    // como saber todos os departamentos??
+    let codDepto = 0;
+    for (i=0;i<listaProdutos.length; i++){
+        let produto = listaProdutos[i];
+        if (produto.departamento.idDepto != codDepto){
+            //console.log("Encontrei novo departamento = "+produto.departamento.nomeDepto);
+            // vou definir uma pequena estrutura para armazenar na lista, contendo o nome do departamento e inicialmente a quantidade zerada
+            let itemLista = {
+                nomeDepto     : produto.departamento.nomeDepto,
+                idDepto       : produto.departamento.idDepto,
+                somatoriaItens: 0,
+                totalEstoque  : 0,
+                ticketMedio   : 0
+            };
+            listaDeptos.push(itemLista);
+            codDepto = produto.departamento.idDepto;
+        }
+    }
+
+    // para cada produto, percorrer cada um dos departamentos da lista, fazendo as devidas somatórias (tanto de quantidade de itens, quanto de preço)
+    for (i=0; i<listaProdutos.length; i++){
+        let produto = listaProdutos[i];
+        for (j=0; j<listaDeptos.length; j++){
+            if (produto.departamento.idDepto == listaDeptos[j].idDepto){
+                listaDeptos[j].somatoriaItens += produto.qtdEstoque;
+                listaDeptos[j].totalEstoque   += produto.preco * produto.qtdEstoque;
+                break;
+            }
+        }
+    }
+
+    // defini todos os valores de somatoria de itens e também de total do estoque... para facilitar a compreensão,
+    // vamos pela última vez, percorrer a lista de departamentos e calcular o ticket médio
+
+    for (j=0; j<listaDeptos.length; j++){
+        listaDeptos[j].ticketMedio = listaDeptos[j].totalEstoque / listaDeptos[j].somatoriaItens;
+    }
+
+    console.log(listaDeptos);
 
 
-
-/*exercicio 5*/
-
-    const totalInventario = listaProdutos.reduce((prevVal,elem) => prevVal+elem.preco, 0 ) * 
-    listaProdutos.reduce((prevVal,elem) => prevVal+elem.qtdEstoque, 0 )
-    console.log('Valor Total Inventario',totalInventario);
-
- /*exercicio 6 */
- 
-    const maisValioso = listaProdutos.reduce(function(acumulador, valor){
-     if (acumulador.preco > valor.preco) return acumulador;
-     return valor; 
- });
- console.log(maisValioso)
-
- /* exerciocio 7*/
-
- const maisBarato = listaProdutos.reduce(function(acumulador, valor){
-    if (acumulador.preco < valor.preco) return acumulador;
-    return valor; 
-});
-console.log(maisBarato)
-
-/* exerciocio 8*/
-
-
+}*/
